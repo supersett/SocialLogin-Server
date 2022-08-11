63b53c70f1b85652ceb9631be9a6fdb99947046a
Hyunwoo Jung <92968138+supersett@users.noreply.github.com>
2022-08-11 15:38:50 +0900

    Delete drftest directory

--- drftest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import CoordinateCreateAPIView,CoordinateDetatilAPIView
-
-urlpatterns = [
-    path("coordination/", CoordinateCreateAPIView.as_view(), name='CoordinateCreateAPIView'),
-    path("coordination/<int:pk>", CoordinateDetatilAPIView.as_view(), name='CoordinateDetatilAPIView')
-]
