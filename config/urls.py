bdc8d6dfe4cb3a786d3226dcb5910eaed1baabec
supersett <nolang3748@naver.com>
2022-08-11 15:41:29 +0900

    feat : social login

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/', include('dj_rest_auth.registration.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
+]
